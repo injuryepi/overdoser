f2b8d8ced2729066ad7663d940f5305a96264670
injuryepi <injury.epi@gmail.com>
2017-09-30 14:19:40 -0700

    Add files via upload

---
+++ R/od_crude_rate.R
@@ -0,0 +1,25 @@
+od_crude_rate <- function(data, count, population, s = 100000, r = 1, alpha = 0.05 ){
+
+	# confidence intervals with Wilson method
+	#simpler than the direct method
+
+	count <- enquo(count)
+	population <- enquo(population)
+
+	suppressWarnings(suppressMessages(require(dplyr)))
+	suppressWarnings(suppressMessages(require(tibble)))
+
+	a <- data %>% pull(count)
+	N <- data %>% pull(population)
+	p <- a/N
+	z <- -qnorm(alpha/2)
+	moe <- ((z/sqrt(N))*sqrt(p*(1-p) + z^2/(4*N)))/(1+z^2/N)
+	center <- (p+z^2/(2*N))/(1+z^2/N)
+	lower_limit <- center-moe
+	upper_limit <- center+moe
+
+	data %>%
+		add_column(crude_rate = round(p*s, r) ,
+							 lower_crude_rate = round(lower_limit*s, r),
+							 upper_crude_rate = round(upper_limit*s, r))
+}
