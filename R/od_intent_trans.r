3da64ca97a71c53585090748b7ae0fe3001cb221
injuryepi <injury.epi@gmail.com>
2017-09-30 16:08:11 -0700

    Create od_intent_trans.r
    intent  whether icd-9 or icd-10

---
+++ R/od_intent_trans.r
@@ -0,0 +1,17 @@
+od_intent_trans <- function(data, diag_ecode_col, date){
+
+	date <- enquo(date)
+
+	data1 <- data %>%
+		filter(od_fed_fiscal_year(!!date) < 2016) %>%
+		od_intent_icd9cm(diag_ecode_col = diag_ecode_col)
+
+	data2 <- data %>%
+		filter(od_fed_fiscal_year(!!date) > 2015) %>%
+		od_intent_icd10cm(diag_ecode_col = diag_ecode_col)
+
+	data <- dplyr::bind_rows(data1, data2)
+
+	data
+
+}
