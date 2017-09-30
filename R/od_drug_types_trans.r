e04756353532cad4b684836e08296c3cc443672d
injuryepi <injury.epi@gmail.com>
2017-09-30 15:03:06 -0700

    Create od_drug_types_trans.r
    combine drug types for pre fiscal 2016 and after

---
+++ R/od_drug_types_trans.r
@@ -0,0 +1,23 @@
+od_drug_types_trans <- function(data, diag_ecode_col, date){
+
+
+	data1 <- data %>% 
+		filter(od_fed_fiscal_year(date) < 2016) %>% 
+		od_drug_types_icd9cm(diag_ecode_col = diag_ecode_col) %>% 
+		rename(any_drug = any_drug_icd9cm,
+					 any_opioid = any_opioid_icd9cm,
+					 non_heroin_opioid = non_heroin_icd9cm,
+					 heroin = heroin_icd9cm)
+	
+	data2 <- data %>% 
+		filter(od_fed_fiscal_year(date) > 2015) %>% 
+		od_drug_types_icd10cm(diag_ecode_col = diag_ecode_col) %>% 
+		rename(any_drug = any_drug_icd10cm,
+					 any_opioid = any_opioid_icd10cm,
+					 non_heroin_opioid = non_heroin_icd10cm,
+					 heroin = heroin_icd10cm)
+	
+	data <- dplyr::bind_rows(data1, data2)
+		
+	data
+}
