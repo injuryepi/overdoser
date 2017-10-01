13e962e946751cfa4f88b4054e7688e1226cd7bf
injuryepi <injury.epi@gmail.com>
2017-09-30 17:11:41 -0700

    Update od_fatal_any_drug.R

--- R/od_fatal_any_drug.R
+++ R/od_fatal_any_drug.R
@@ -1,6 +1,6 @@
 od_fatal_any_drug <- function(data, underly_col){
   data %>%
     mutate(cdc_any_drug =
-            create_diag(., expr = "X4[0-4]|X6[0-4]|X85|Y1[0-4]",
+            od_create_diag(., expr = "X4[0-4]|X6[0-4]|X85|Y1[0-4]",
                         colvec = underly_col))
 }
