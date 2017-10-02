f8285e678da9774b0baca8d298b35ea9a7b4fb4f
injuryepi <injury.epi@gmail.com>
2017-10-01 17:31:00 -0700

    Add files via upload

--- R/od_date_day.r
+++ R/od_date_day.r
@@ -1,4 +1,16 @@
+#' Title
+#'
+#' @param x
+#'
+#' @return Return complete date with 15/ inserted as day
+#' @export
+#'
+#' @examples
+#' od_full_date(c("03/2015", "11/2014", "09/2017"))
+#' # "2015-03-15" "2014-11-15" "2017-09-15"
 od_date_day <- function(x){
 	# add 15/ between month and year for truncated date
+	suppressWarnings(suppressMessages(require(lubridate)))
+
 	mdy(gsub( "(?<=^.{3})", "15/", x, perl=TRUE ))
 }
