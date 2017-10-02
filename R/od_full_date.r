bb9ab7d6d67a5ce4c0666d0451ff3c2e066006cd
injuryepi <injury.epi@gmail.com>
2017-10-01 17:14:13 -0700

    Update od_full_date.r

--- R/od_full_date.r
+++ R/od_full_date.r
@@ -1,6 +1,4 @@
 od_full_date <- function(x){
 	# add 15/ between month and year for truncated date
-	suppressWarnings(suppressMessages(require(lubridate)))
-
 	mdy(gsub( "(?<=^.{3})", "15/", x, perl=TRUE ))
 }
