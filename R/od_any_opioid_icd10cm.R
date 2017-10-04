b3d6c4ad8643b584796d8b1d7bdf445c72c627fb
injuryepi <injury.epi@gmail.com>
2017-10-04 12:46:14 -0700

    Update od_any_opioid_icd10cm.R
    Add possibility to assign name to the created variable

--- R/od_any_opioid_icd10cm.R
+++ R/od_any_opioid_icd10cm.R
@@ -3,14 +3,16 @@
 #' @param data: input data
 #' @param diag_ecode_col: indices of diagnosis and
 #' E codes of interest
+#' @param any_opioid_icd10cm: choose the name for the created variable 
 #'
-#' @return variable any_opioid_icd10cm
+#' @return variable any_opioid_icd10cm (or the chosen name
 #' @export
 #'
 #' @examples to be added
-add_any_opioid_icd10cm <- function(data, diag_ecode_col) {
-    
+od_any_opioid_icd10cm <- function(data, diag_ecode_col, any_opioid_icd10cm) {
+    any_opioid_icd10cm <- enquo(any_opioid_icd10cm)
+  any_opioid_icd10cm_name <- quo_name(any_opioid_icd10cm)
     cdc_opioid_icd10cm_regex7_ <- "(T40[01234].|T406[09])(1|2|3|4)(A|D|$)"
-    data %>% mutate(any_opioid_icd10cm = od_create_diag(., expr = cdc_opioid_icd10cm_regex7_, 
+    data %>% mutate(!!any_opioid_icd10cm_name := od_create_diag(., expr = cdc_opioid_icd10cm_regex7_, 
         colvec = diag_ecode_col))
 }
