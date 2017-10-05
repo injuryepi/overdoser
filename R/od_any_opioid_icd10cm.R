060044b3f510c5f28407808d92f43a678b1c03c0
injuryepi <injury.epi@gmail.com>
2017-10-04 18:01:12 -0700

    Update od_any_opioid_icd10cm.R
    add any_opioid_icd10cm as the default name

--- R/od_any_opioid_icd10cm.R
+++ R/od_any_opioid_icd10cm.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples to be added
-od_any_opioid_icd10cm <- function(data, diag_ecode_col, any_opioid_icd10cm) {
+od_any_opioid_icd10cm <- function(data, diag_ecode_col, any_opioid_icd10cm = any_opioid_icd10cm) {
 	any_opioid_icd10cm <- enquo(any_opioid_icd10cm)
 	any_opioid_icd10cm_name <- quo_name(any_opioid_icd10cm)
 	cdc_opioid_icd10cm_regex7_ <- "(T40[01234].|T406[09])(1|2|3|4)(A|D|$)"
