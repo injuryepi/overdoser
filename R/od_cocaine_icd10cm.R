fb8fb4e844995aa319e0bb1b57d45764094c4e7e
injuryepi <injury.epi@gmail.com>
2017-10-09 17:12:15 -0700

    Add files via upload

---
+++ R/od_cocaine_icd10cm.R
@@ -0,0 +1,16 @@
+#' Find cocain based on ICD-10-CM.
+#'
+#' Find cocaine based on ICD-10-CM.
+#'
+#' @param data input data
+#' @param diag_ecode_col column indices
+#'
+#' @return cocaine_icd10cm
+#' @export
+#'
+#' @examples to be added
+od_cocaine_icd10cm <- function(data, diag_ecode_col) {
+
+	cdc_cocaine_icd10cm_regex7_ <- "T405.(1|2|3|4)(A|D|$)"
+	data %>% mutate(heroin_icd10cm = create_diag(., expr = cdc_cocaine_icd10cm_regex7_, colvec = diag_ecode_col))
+}
