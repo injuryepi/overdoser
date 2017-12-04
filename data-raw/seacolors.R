c9706d23e3ae15ae98be9a020d3584ade3ccb4bc
injuryepi <injury.epi@gmail.com>
2017-12-03 17:52:30 -0800

    Add files via upload

--- data-raw/seacolors.R
+++ data-raw/seacolors.R
@@ -1,10 +1,19 @@
 #https://teamcolorcodes.com/seattle-seahawks-color-codes/
 
-col_names <- c("college_navy", "action_green", "wolf_gray", "seahawks_blue")
-seacolors <- c("#001433", "#4DFF00", "#A5ACAF", "#245998")
+#https://teamcolorcodes.com/seattle-sounders-fc-color-codes/
+
 library(tidyverse)
-seacolors <- seacolors %>%
-	set_names(col_names)
+
+hawks_names <- c("college_navy", "action_green", "wolf_gray", "seahawks_blue")
+seahawks <- c("#001433", "#4DFF00", "#A5ACAF", "#245998") %>%
+	set_names(hawks_names)
+
+sounders_names <- c("sounder_blue", "rave_green", "cascade_shale")
+sounders <- c("#005695", "#5D9732", "#132530") %>%
+	set_names(sounders_names)
+
+seacolors <- c(seahawks, sounders)
+
 
 
 devtools::use_data(seacolors , overwrite = T)
