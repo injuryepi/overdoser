ed309efddc8404c3208651aab191c04b31a6027e
injuryepi <injury.epi@gmail.com>
2018-01-16 18:43:10 -0800

    Update od_create_diag.r
    allows the use of variable names without quotation marks

--- R/od_create_diag.r
+++ R/od_create_diag.r
@@ -20,14 +20,16 @@
 #' \tcolvec = c(1:5)) %>% sample_n(10)
 #'
 od_create_diag <- function(data, expr, colvec, ignore.case = T, perl = T) {
-   require(dplyr, quietly = T)
-  require(tidyr, quietly = T)
-  require(purrr, quietly = T)
-  #a function to assign '1' if the regular expression matched 
+  #expr = regular expressions 
+  # colvec = vector of the columns of interest (columns with the diagnoses). indices
+  # or variable names without quotation marks
+   colvec = enquo(colvec)
+  # assign '1' if the regular expression matched 
   f1 = function(x) as.numeric(grepl(expr, x, ignore.case = ignore.case, perl = perl))
+  # any one in the diagnosis field suffices
   f2 = function(x){as.numeric(rowSums(x, na.rm = TRUE) > 0)} 
  
-  data %>% select(colvec) %>% 
+  data %>% select(!!colvec) %>% 
     mutate_all(funs(as.character)) %>% 
     map_df(f1) %>% 
     mutate(new_diag = f2(.)) %>% 
