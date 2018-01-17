61bc4827a6e17358528b9a23587952c31c64cbb0
injuryepi <injury.epi@gmail.com>
2018-01-16 18:44:38 -0800

    Update od_create_cond_diag.R
    allows the use of variable names without quotation marks

--- R/od_create_cond_diag.R
+++ R/od_create_cond_diag.R
@@ -18,10 +18,11 @@ od_create_cond_diag <- function(data, expr, colvec, ignore.case = T, perl = T,
     # expr=regular expressions of interest colvec = vector of the columns of
     # interest (columns with the diagnosis code) cond.var = the conditional
     # column with 0 and 1
+    colvec = enquo(colvec)
     cond_var = enquo(cond.var)
   
   data %>% 
-    mutate(new_diag = od_create_diag(., expr = expr, colvec = colvec, ignore.case = ignore.case, perl = perl)) %>%  
+    mutate(new_diag = od_create_diag(., expr = expr, colvec = !!colvec, ignore.case = ignore.case, perl = perl)) %>%  
     mutate_at(vars(new_diag), funs(ifelse((!!cond_var) == 1, new_diag, 0 ))) %>% pull(new_diag)
 
 }
