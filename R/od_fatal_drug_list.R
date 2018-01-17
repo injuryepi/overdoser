c577961de83e96fd504b5eadfca7ff6bd5642cec
injuryepi <injury.epi@gmail.com>
2018-01-17 11:48:02 -0800

    Update od_fatal_drug_list.R
    adding non-heroin opioid

--- R/od_fatal_drug_list.R
+++ R/od_fatal_drug_list.R
@@ -28,6 +28,7 @@ od_fatal_drug_list <- function(data, underly_col, mult_col) {
     data %>% mutate(cdc_any_drugs = od_create_diag(., expr = "X4[0-4]|X6[0-4]|X85|Y1[0-4]", 
         colvec = underly_col)) %>% mutate(
         cdc_any_opioid = od_create_cond_diag(., expr = "T40[0-46]", colvec = mult_col, cond.var = cdc_any_drugs), 
+        non_heroin_opioid = od_create_cond_diag(., expr = "T40[02-46]", colvec = mult_col, cond.var = cdc_any_drugs),
         cdc_heroin_t40_1 = od_create_cond_diag(.,expr = "T401", colvec = mult_col, cond.var = cdc_any_drugs), 
         cdc_opioid_t40_2 = od_create_cond_diag(.,expr = "T402", colvec = mult_col, cond.var = cdc_any_drugs), 
         cdc_opioid_t40_3 = od_create_cond_diag(., expr = "T403", colvec = mult_col, cond.var = cdc_any_drugs), 
