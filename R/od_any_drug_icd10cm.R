efd2b1179c53d6d3e3506fe44199db2759cfa1fd
injuryepi <injury.epi@gmail.com>
2018-04-23 18:12:44 -0700

    Update od_any_drug_icd10cm.R
    remove encounter D

--- R/od_any_drug_icd10cm.R
+++ R/od_any_drug_icd10cm.R
@@ -15,7 +15,7 @@
 #' hosp_set %>% od_any_drug_icd10cm(diag_ecode_col = c(3,6)) %>% sample_n(10)
 od_any_drug_icd10cm <- function(data, diag_ecode_col) {
     
-    cdc_drugs_icd10cm_regex7_ <- "(?!(T3[679]9|T414|T427|T4[3579]9))(T3[6-9]|T4[0-9]|T50)..(1|2|3|4)(A|D|$)|((T3[679]9|T414|T427|T4[3579]9)(1|2|3|4).(A|D|$))"
+    cdc_drugs_icd10cm_regex7_ <- "(?!(T3[679]9|T414|T427|T4[3579]9))(T3[6-9]|T4[0-9]|T50)..(1|2|3|4)(A|D|$)|((T3[679]9|T414|T427|T4[3579]9)(1|2|3|4).(A|$))"
     data %>% mutate(any_drug_icd10cm = od_create_diag(., expr = cdc_drugs_icd10cm_regex7_, 
         colvec = diag_ecode_col))
 }
