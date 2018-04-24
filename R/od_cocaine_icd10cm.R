e2382b3190c16f9c0b7f8fa76f23061ee2aaca96
injuryepi <injury.epi@gmail.com>
2018-04-23 18:33:52 -0700

    Update od_cocaine_icd10cm.R

--- R/od_cocaine_icd10cm.R
+++ R/od_cocaine_icd10cm.R
@@ -11,6 +11,6 @@
 #' @examples to be added
 od_cocaine_icd10cm <- function(data, diag_ecode_col) {
 
-	cdc_cocaine_icd10cm_regex7_ <- "T405.(1|2|3|4)(A|D|$)"
+	cdc_cocaine_icd10cm_regex7_ <- "T405.(1|2|3|4)(A|$)"
 	data %>% mutate(cocaine_icd10cm = od_create_diag(., expr = cdc_cocaine_icd10cm_regex7_, colvec = diag_ecode_col))
 }
