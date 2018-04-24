9b0082d792b4add2b245dfe329754539ffd74728
injuryepi <injury.epi@gmail.com>
2018-04-23 18:33:03 -0700

    Update od_any_opioid_icd10cm.R

--- R/od_any_opioid_icd10cm.R
+++ R/od_any_opioid_icd10cm.R
@@ -12,7 +12,7 @@
 od_any_opioid_icd10cm <- function(data, diag_ecode_col, any_opioid_icd10cm = any_opioid_icd10cm) {
 	any_opioid_icd10cm <- enquo(any_opioid_icd10cm)
 	any_opioid_icd10cm_name <- quo_name(any_opioid_icd10cm)
-	cdc_opioid_icd10cm_regex7_ <- "(T40[01234].|T406[09])(1|2|3|4)(A|D|$)"
+	cdc_opioid_icd10cm_regex7_ <- "(T40[01234].|T406[09])(1|2|3|4)(A|$)"
 	data %>% mutate(!!any_opioid_icd10cm_name := od_create_diag(., expr = cdc_opioid_icd10cm_regex7_,
 																															colvec = diag_ecode_col))
 }
