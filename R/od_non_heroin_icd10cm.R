6802389cd1634c37f68cd2a88ea373dae36c1e38
injuryepi <injury.epi@gmail.com>
2018-04-23 18:43:04 -0700

    Update od_non_heroin_icd10cm.R
    remove encounter D

--- R/od_non_heroin_icd10cm.R
+++ R/od_non_heroin_icd10cm.R
@@ -9,7 +9,7 @@
 #' @examples to be added
 od_non_heroin_icd10cm <- function(data, diag_ecode_col) {
 
-    cdc_non_heroin_icd10cm_regex7_ <- "(T40[0234].|T406[09])(1|2|3|4)(A|D|$)"
+    cdc_non_heroin_icd10cm_regex7_ <- "(T40[0234].|T406[09])(1|2|3|4)(A|$)"
     data %>% mutate(non_heroin_icd10cm = od_create_diag(., expr = cdc_non_heroin_icd10cm_regex7_,
         colvec = diag_ecode_col))
 }
