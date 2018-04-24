60f6be254ceb98829f2e1ed1156df8713efe10d4
injuryepi <injury.epi@gmail.com>
2018-04-23 18:40:51 -0700

    Update od_heroin_icd10cm.R
    remove encounter D

--- R/od_heroin_icd10cm.R
+++ R/od_heroin_icd10cm.R
@@ -11,7 +11,7 @@
 #' @examples to be added
 od_heroin_icd10cm <- function(data, diag_ecode_col) {
     
-    cdc_heroin_icd10cm_regex7_ <- "T401.(1|2|3|4)(A|D|$)"
+    cdc_heroin_icd10cm_regex7_ <- "T401.(1|2|3|4)(A|$)"
     data %>% mutate(heroin_icd10cm = create_diag(., expr = cdc_heroin_icd10cm_regex7_, 
         colvec = diag_ecode_col))
 }
