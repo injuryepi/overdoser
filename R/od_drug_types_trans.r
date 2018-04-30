ed41bd7ecc0036fc3e4ce00605221e5fe8e23f65
injuryepi <injury.epi@gmail.com>
2018-04-29 18:56:06 -0700

    Update od_drug_types_trans.r
    Add stopifnot to assert having icd-9 cm and icd-10 cm
    Separate the arguments for icd10cm and icd9cm columns

--- R/od_drug_types_trans.r
+++ R/od_drug_types_trans.r
@@ -15,20 +15,24 @@
 #' hosp_set %>%
 #' od_drug_types_trans(diag_ecode_col = c(3, 6), date = discharge_date) %>% select(-age, -diagnosis_2, -diagnosis_3, -ecode2, -year) %>% sample_n(10)
 #'
-od_drug_types_trans <- function(data, diag_ecode_col, date) {
+od_drug_types_trans <- function(data, diag_ecode_col_9, diag_ecode_col_10, date) {
 
 	date <- enquo(date)
+	# pull the fiscal years
+	year_f <- data %>% pull(od_fed_fiscal_year(!!date))
+	# assert that pre 2016 fiscal year and after 2015 fiscal year are both included
+	stopifnot(any(year_f < 2016) & any(year_f > 2015))
 
-	data1 <- data %>% filter(od_fed_fiscal_year(!!date) < 2016) %>% od_drug_types_icd9cm(diag_ecode_col = diag_ecode_col) %>%
+	data1 <- data %>% filter(od_fed_fiscal_year(!!date) < 2016) %>% od_drug_types_icd9cm(diag_ecode_col = diag_ecode_col_9) %>%
 		rename(any_drug = any_drug_icd9cm, any_opioid = any_opioid_icd9cm,
 					 non_heroin_opioid = non_heroin_icd9cm, heroin = heroin_icd9cm)
 
-	data2 <- data %>% filter(od_fed_fiscal_year(!!date) > 2015) %>% od_drug_types_icd10cm(diag_ecode_col = diag_ecode_col) %>%
+	data2 <- data %>% filter(od_fed_fiscal_year(!!date) > 2015) %>% od_drug_types_icd10cm(diag_ecode_col = diag_ecode_col_10) %>%
 		rename(any_drug = any_drug_icd10cm, any_opioid = any_opioid_icd10cm,
 					 non_heroin_opioid = non_heroin_icd10cm, heroin = heroin_icd10cm) %>%
 	        select(-opioid_t40_2_icd10cm, -opioid_t40_3_icd10cm, -opioid_t40_4_icd10cm, -cocaine_t40_5_icd10cm, -stimulant_t43_6_icd10cm)
 
 	data <- dplyr::bind_rows(data1, data2)
 
-	data
+	data %>% mutate(non_heroin_opioid = ifelse(heroin == 1, 0, non_heroin_opioid)) 
 }
