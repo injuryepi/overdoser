8a491c60f41d534dffa7ac9a59b8e3ea6d5d7a6b
injuryepi <injury.epi@gmail.com>
2018-05-01 18:05:42 -0700

    Update od_fed_fiscal_year.r
    remove 'stopifnot(is.Date(date))'

--- R/od_fed_fiscal_year.r
+++ R/od_fed_fiscal_year.r
@@ -13,6 +13,6 @@
 od_fed_fiscal_year <- function(date) {
     # October 1, year_n to September 30, year_n+1
     require(lubridate, quietly = T)
-    stopifnot(is.Date(date))  # check the date format
+    # stopifnot(is.Date(date))  # check the date format
     year(date %m+% months(3))
 }
