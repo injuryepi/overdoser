535ac6adf79221deeff0dae0620c2466e83bd970
injuryepi <injury.epi@gmail.com>
2018-05-02 16:44:35 -0700

    Update od_ui_fatal_opioid.r

--- R/od_ui_fatal_opioid.r
+++ R/od_ui_fatal_opioid.r
@@ -13,7 +13,7 @@
 #' @examples to be added
 od_ui_fatal_opioid <- function(data, underly_col, mult_col) {
     data %>% mutate(ui_drug = od_create_diag(., expr = "X4[0-4]", 
-        colvec = underly_col)) %>% mutate(cdc_any_opioid = od_create_cond_diag(., 
+        colvec = underly_col)) %>% mutate(ui_opioid = od_create_cond_diag(., 
         expr = "T40[0-46]", colvec = mult_col, cond.var = ui_drug)) %>% 
         select(-ui_drug)
 }
