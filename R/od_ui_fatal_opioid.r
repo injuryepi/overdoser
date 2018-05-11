1dad22b7082d0844458faf8f66a8aef962f5a3c9
injuryepi <injury.epi@gmail.com>
2018-05-10 19:31:41 -0700

    Add files via upload

--- R/od_ui_fatal_opioid.r
+++ R/od_ui_fatal_opioid.r
@@ -5,15 +5,25 @@
 #' @param data input data
 #' @param underly_col underlying column index
 #' @param mult_col multiple causes column indices
+#' @param var_name propose a name for the new alcohol variable. The default is "ui_opioid"
 #'
 #' @return cdc_any_drugs, cdc_any_opioid,
 #'
 #' @export
 #'
 #' @examples to be added
-od_ui_fatal_opioid <- function(data, underly_col, mult_col) {
-    data %>% mutate(ui_drug = od_create_diag(., expr = "X4[0-4]", 
-        colvec = underly_col)) %>% mutate(ui_opioid = od_create_cond_diag(., 
-        expr = "T40[0-46]", colvec = mult_col, cond.var = ui_drug)) %>% 
-        select(-ui_drug)
+#'
+#'
+od_ui_fatal_opioid <- function (data, underly_col, mult_col, var_name = "ui_opioid"){
+	var_name <- enquo(var_name)
+	var_name <- quo_name(var_name)
+	data %>% mutate(ui_drug = od_create_diag(., expr = "X4[0-4]",
+																					 colvec = underly_col)) %>%
+		mutate(!!var_name :=  od_create_cond_diag(.,
+																					 expr = "T40[0-46]",
+																					 colvec = mult_col,
+																					 cond.var = ui_drug)) %>%
+		select(-ui_drug)
 }
+
+
