c81e4c256f9951afd7f2c373b1cb373a9dbfb84b
injuryepi <injury.epi@gmail.com>
2018-08-04 20:25:29 -0700

    Update od_fatal_alcohol.R

--- R/od_fatal_alcohol.R
+++ R/od_fatal_alcohol.R
@@ -13,7 +13,6 @@
 od_fatal_alcohol <- function(data, underly_col, var_name = "alcohol") {
 	var_name <- enquo(var_name)
 	var_name <- quo_name(var_name)
-	data %>% mutate(!!var_name :=  od_create_diag(., expr = "X45|X65|Y15",
-																								colvec = underly_col))
+	data %>% mutate(!!var_name :=  od_create_diag(., expr = "X45|X65|Y15", colvec = underly_col))
 }
 
